dd48aff048417fa703076b2cdea4415ccb5d84d4
DESKTOP-EBAUVSA\grant <incarnate6@gmail.com>
2021-11-16 21:28:11 +0300

    succes register

--- src/library/api/auth.ts
+++ src/library/api/auth.ts
@@ -1,6 +1,5 @@
 import xhr from 'core/axios/config';
 
-export const registerPhone = (data: { phonenumber: string }) => xhr.post('/api/v1/auth/patient/register_phone', data);
+export const registerPhone = (data: { phonenumber: string }) => xhr.post('/auth/patient/register_phone/', data);
 
-export const loginEmail = (data: { username: string; password: string }) =>
-	xhr.post('/api/v1/auth/rest-auth/login/', data);
+export const loginEmail = (data: { username: string; password: string }) => xhr.post('/auth/rest-auth/login/', data);
